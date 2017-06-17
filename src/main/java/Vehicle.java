558f812f3502aa478fd2157bf129356eee4e9702
bfeldsar <bf47667@fer.hr>
2017-06-17 17:47:46 +0200

    Added map(day , requests) in vehicle

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,20 +1,30 @@
 package main.java;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by mmatak on 6/16/17.
  */
 public class Vehicle {
-    private List<Request> requestList = new LinkedList<>();
+    public Map<Integer, List<Request>> dayRequestsMap = new HashMap<>();
+    private List<Request> requestList = new ArrayList<>();
     public int distance;
 
     public void addRequest(Request newRequest){
+        List<Request> requestInDay = null;
+        if (!dayRequestsMap.containsKey(newRequest.pickedDayForDelivery)){
+            requestInDay = new ArrayList<>();
+            dayRequestsMap.put(newRequest.pickedDayForDelivery, requestInDay);
+        }else {
+            requestInDay = dayRequestsMap.get(newRequest.pickedDayForDelivery);
+        }
+        requestInDay.add(newRequest);
         requestList.add(newRequest);
     }
 
     public void removeRequest(Request request){
+        List<Request> requestsDay = dayRequestsMap.get(request.pickedDayForDelivery);
+        requestsDay.remove(request);
         requestList.remove(request);
     }
 
